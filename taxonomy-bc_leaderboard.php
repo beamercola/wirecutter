519c7c05103cb7d01c3f07341eb2e0d7af92836e
Craige Thomas <craiget@gmail.com>
2012-02-05 14:37:02 -0500

    Guides loop in sidebar

--- taxonomy-bc_leaderboard.php
+++ taxonomy-bc_leaderboard.php
@@ -4,7 +4,14 @@
   <h1 class="page-title">Leaderboard: <?php single_tag_title() ?></h1>
 
   <ul class="category-leaderboard">
-    <?php while ( have_posts() ): the_post(); ?>
+    <?php
+      global $post; 
+      while ( have_posts() ): the_post();
+        // hack to skip wc_guides, works for now, alternative is tweaking $wp_query or using a custom loop
+        if( $post->post_type != 'bc_review' ) {
+          continue;
+        }
+    ?>
     <li <?php post_class(); ?>>
       <?php
         $attachments = get_posts(array( 'post_type' => 'attachment', 'numberposts' => -1, 'orderby' => 'menu_order', 'order' => 'asc', 'post_status' => null, 'post_parent' => $post->ID ));
@@ -30,8 +37,24 @@
 
 <div id="side">
   <div class="side-wrapper">
-    LIST OF GUIDES HERE
+    <?php
+    // list all guides having this leaderboard category
+    $term = get_term_by( 'slug', get_query_var( 'term' ), get_query_var( 'taxonomy' ) );
+    $slug = $term->slug;
+    $guides_query = new WP_Query(array('post_type' => 'wc_guide', 'bc_leaderboard' => $slug ));
+    if( $guides_query->have_posts() ):
+      while ( $guides_query->have_posts() ): $guides_query->the_post();
+    ?>
+      <div>
+        <h3><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h3>
+        <div><?php the_content() ?></div>
+      </div>
+      <?php endwhile; ?>
+    <?php else: ?>
+      <p>No guides</p>
+    <?php endif; ?>
   </div>
 </div>
     
 <?php get_footer(); ?>
+
